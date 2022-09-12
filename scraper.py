fe80e1ce042ba951283d807bdc108cfaab9581e0
Ze1DoS <113006937+Ze1DoS@users.noreply.github.com>
2022-09-12 15:05:29 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,3 +1,47 @@
+'''
+Сделано в соответствии с курсом:
+https://www.youtube.com/watch?v=bC-ZQCE7qEE&list=PLZXVHsCfRw5rX3QklmCwaopTO_bLcSHRt&index=6
+'''
+
+import time
+import requests
+from bs4 import BeautifulSoup as BS
+
+def get_links(url):
+    url='https://nextgame.net/catalog/sony-playstation4/' 
+    req=requests.get(url)
+
+    bs = BS(req.text, 'html.parser')
+    items=bs.find_all('div', {'class':'item_block'})
+    links=[]
+    for item in items:
+            links.append('https://nextgame.net' + item.a['href'])
+    return links 
+
+def get_detail(url):
+    req=requests.get(url)
+    bs = BS(req.text, 'html.parser')
+    title = bs.find('h1', {'id':'pagetitle'}).text.strip()
+    article = bs.find('div', {'class': 'article'}).find('span', {'class': 'article__value'}).text.strip()
+    price = bs.find('span', {'class': 'price_value'}).text.strip()
+
+    print('Название:', title)
+    print('Арт.:', article)
+    print('Цена:', price)
+    print('Ссылка:', url)
+
+
+
+url = 'https://nextgame.net/catalog/sony-playstation4/'
+for link in get_links(url):
+    get_detail(link)
+    print('-'*10)
+    time.sleep(2)
+
+#ПРЕДЫДУЩИЙ ВАРИАНТ
+#Возникли сложности со скрапингом описания, которое находится по другой ссылке, ошибку понял, но решил делать в точности по курсу, во избежание, так сказать
+
+'''
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,3 +64,4 @@ while params['page'] <= pages:
     last_page_num = int(soup.find_all('a', class_='page-link')[-2].text)
     pages = last_page_num if pages < last_page_num else pages
     params['page'] += 1
+    '''
